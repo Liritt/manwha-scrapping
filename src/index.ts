5d49e522fa3d4e0879b9c78237d5cb3ffd138b36
Lirit <gaetanl2004@gmail.com>
2023-01-30 19:45:58 +0100

    Suppression de commentaires inutiles

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import {getData} from "./data";
 
 getData()
-/*import {utcToZonedTime, zonedTimeToUtc} from "date-fns-tz";
-
-console.log(new Date("Jan 31, 2023 01:35 UTC+7"));
-let str = "Jan 31,2023 - 01:51 AM".replace("- ", "");
-str = str.substring(0, str.length-2) + "UTF+7";
-console.log(new Date("Jan 29,2023 22:48 UTF+7"))
-
-*/
