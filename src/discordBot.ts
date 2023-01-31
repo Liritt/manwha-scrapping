a15437b7c5ed59c74d4eb978dd14a10bbf255385
Lirit <gaetanl2004@gmail.com>
2023-01-31 20:47:15 +0100

    Ajout du bot discord permettant de récupérer les messages du serveur

--- src/discordBot.ts
+++ src/discordBot.ts
@@ -1,18 +1,51 @@
-import * as discord from "discord.js";
-import * as dotenv from "dotenv";
-import "../types";
+import {Client, GatewayIntentBits, TextChannel} from "discord.js";
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const bot = new discord.Client({intents: [3243773]});
-
-bot.once("ready", () => {
-    console.log("Je suis en ligne maintenant !")
-})
-
-bot.on("messageCreate", message => {
-    if (message.author.bot) return;
-    if (message.author.id === process.env.AUTHOR_ID && message.channel.id === process.env.CHANNEL_ID) message.channel.send("Message reçu !");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
-bot.login(process.env.TOKEN);
+client.once("ready", async () => {
+    console.log("Je suis en ligne maintenant !")
+    const guild = client.guilds.cache.get(process.env.SERVER_ID);
+    if (!guild) return console.error('Guild not found');
+
+    const channel = guild.channels.cache.get(process.env.CHANNEL_ID) as TextChannel;
+    if (!channel) return console.error('Text channel not found');
+
+    const messages = await channel.messages.fetch({ limit: 5 });
+    console.log(`Received ${messages.size} messages`);
+    messages.forEach((message) => {
+        if (message.author.id === process.env.AUTHOR_ID) {
+            console.log(message.content)
+        }
+    });
+})
+
+client.on('messageUpdate', async (oldMessage, newMessage) => {
+    if (newMessage.author.id === process.env.AUTHOR_ID && newMessage.guild.id === process.env.SERVER_ID && newMessage.channel.id === process.env.CHANNEL_ID) {
+        console.log(`Ancien contenu : ${oldMessage.content}`);
+        console.log(`Nouveau contenu : ${newMessage.content}`);
+    }
+});
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+    if (message.author.id === process.env.AUTHOR_ID && (message.channel.id === process.env.CHANNEL_ID || message.channel.id === process.env.CHANNEL_ID2)) console.log(message.content);
+})
+
+client.on("messageDelete", async (message) => {
+    if (message.author.id === process.env.AUTHOR_ID && message.guild.id === process.env.SERVER_ID && message.channel.id === process.env.CHANNEL_ID) {
+        console.log(`Message supprimé: ${message}`)
+    }
+})
+
+
+client.login(process.env.TOKEN);
--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      SERVER_ID: string,
       AUTHOR_ID: string,
       CHANNEL_ID: string,
+      CHANNEL_ID2: string,
       TOKEN: string
     }
   }
