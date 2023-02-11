6fa68322b43739ea9eb1a3051ec9fc58e8e4d942
Lirit <gaetanl2004@gmail.com>
2023-02-11 18:17:11 +0100

    Récupération de tout les messages complétés

--- src/discordBot.ts
+++ src/discordBot.ts
@@ -1,4 +1,4 @@
-import {Client, GatewayIntentBits, TextChannel} from "discord.js";
+import {Client, GatewayIntentBits, Message, TextChannel} from "discord.js";
 import * as dotenv from 'dotenv';
 import { dbConnexion } from '../DBConnexion';
 
@@ -13,6 +13,20 @@ const client = new Client({
     ]
 });
 
+async function getAllMessages(channel: TextChannel) {
+    let lastId: string | undefined;
+    const messages: Array<Message> = [];
+    let messagesChunk;
+    while (true) {
+        const options = lastId ? { limit: 100, before: lastId } : { limit: 100 };
+        messagesChunk = await channel.messages.fetch(options);
+        messages.push(...messagesChunk.values());
+        if (messagesChunk.size < 100) break;
+        lastId = messagesChunk.last()?.id;
+    }
+    return messages;
+}
+
 client.once("ready", async () => {
     console.log("Je suis en ligne maintenant !")
     const guild = client.guilds.cache.get(process.env.SERVER_ID);
@@ -21,54 +35,52 @@ client.once("ready", async () => {
     const channel1 = guild.channels.cache.get(process.env.CHANNEL1_ID) as TextChannel;
     const channel2 = guild.channels.cache.get(process.env.CHANNEL2_ID) as TextChannel;
     if (!channel1 && !channel2) return console.error('Text channel not found');
-
-    const messages1 = await channel1.messages.fetch({ limit: 5 });
-    const messages2 = await channel2.messages.fetch({ limit: 5 });
-    console.log(`Received ${messages1.size + messages2.size} messages`);
-    dbConnexion.connect()
+    const messages1 = await getAllMessages(channel1);
+    const messages2 = await getAllMessages(channel2);
+    console.log(messages2.length);
+    return dbConnexion.connect()
         .then(() => {
-            // TO DO : Créer un trigger qui sur delete remet les id bien comme il faut et remet la sequence en place
             console.log("Connected to database");
-            return Promise.all([...messages1, ...messages2].map(async (message) => {
-                if (message[1].author.id === process.env.AUTHOR_ID) {
-                    try {
-                        let manwhaName: string = message[1].content;
-                        let parsedMessage: number;
-                        let numLastChap = '';
-                        let charToConvert: string;
-                        do {
-                            charToConvert = manwhaName.charAt(manwhaName.length-1);
-                            if (charToConvert === ".") {
-                                numLastChap = charToConvert + numLastChap;
-                                manwhaName = manwhaName.substring(0, manwhaName.length-1);
+            return [...messages1, ...messages2].map(async (message) => {
+                try {
+                    if (message.author.id === process.env.AUTHOR_ID) {
+                        try {
+                            let manwhaName: string = message.content;
+                            let parsedMessage: number;
+                            let numLastChap = '';
+                            let charToConvert: string;
+                            do {
                                 charToConvert = manwhaName.charAt(manwhaName.length-1);
+                                if (charToConvert === ".") {
+                                    numLastChap = charToConvert + numLastChap;
+                                    manwhaName = manwhaName.substring(0, manwhaName.length-1);
+                                    charToConvert = manwhaName.charAt(manwhaName.length-1);
+                                }
+                                parsedMessage = parseInt(charToConvert, 10);
+                                if (!isNaN(parsedMessage)) {
+                                    numLastChap = charToConvert + numLastChap;
+                                    manwhaName = manwhaName.substring(0, manwhaName.length-1).trim();
+                                }
+                            } while (!isNaN(parsedMessage) && manwhaName.length > 0);
+                            const testedManwhaName = manwhaName.toLowerCase();
+                            if (testedManwhaName.endsWith('- chapter') || testedManwhaName.endsWith('- chapitre')) {
+                                manwhaName = manwhaName.substring(0, manwhaName.length-9).trim();
+                            } else if (testedManwhaName.endsWith('chapter') || testedManwhaName.endsWith('chapitre')) {
+                                manwhaName = manwhaName.substring(0, manwhaName.length-7).trim();
                             }
-                            parsedMessage = parseInt(charToConvert, 10);
-                            if (!isNaN(parsedMessage)) {
-                                numLastChap = charToConvert + numLastChap;
-                                console.log(numLastChap)
-                                manwhaName = manwhaName.substring(0, manwhaName.length-1).trim();
+                            if (!((await dbConnexion.query(`SELECT "name" FROM "startedManwha" WHERE "name"=$1`, [manwhaName])).rows.length > 0)) {
+                                await dbConnexion.query(`INSERT INTO "startedManwha" ("name", "numLastChap") VALUES ($1, $2)`, [manwhaName, parseFloat(numLastChap)]);
+                                console.log("Inserting message into database");
                             }
-                        } while (!isNaN(parsedMessage) && manwhaName.length > 0);
-                        const testedManwhaName = manwhaName.toLowerCase();
-                        if (testedManwhaName.endsWith('- chapter')) {
-                            manwhaName = manwhaName.substring(0, manwhaName.length-9).trim();
-                        } else if (testedManwhaName.endsWith('chapter')) {
-                            manwhaName = manwhaName.substring(0, manwhaName.length-7).trim();
+                        } catch (error) {
+                            console.error(error);
                         }
-                        if (!((await dbConnexion.query(`SELECT "name" FROM "startedManwha" WHERE "name"=$1`, [manwhaName])).rows.length > 0)) {
-                            await dbConnexion.query(`INSERT INTO "startedManwha" ("name", "numLastChap") VALUES ($1, $2)`, [manwhaName, parseFloat(numLastChap)]);
-                            console.log("Inserting message into database");
-                        }
-                    } catch (error) {
-                        console.error(error);
                     }
+                } catch (err) {
+                    console.log(message);
                 }
-            }));
+            });
         })
-        .then(() => {
-            dbConnexion.end();
-        });
 });
 
 client.on('messageUpdate', async (oldMessage, newMessage) => {
